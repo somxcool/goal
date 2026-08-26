bee6d04963ceb58f01bac4f648f58a3e06145946
somxcool <decoyemail@duck.com>
2026-08-27 00:58:38 +0500

    Updated goal.py and added .gitignore

--- goal.py
+++ goal.py
@@ -1,1 +1,25 @@
 ## Start
+
+import dearpygui.dearpygui as dpg
+import sqlite3
+
+dpg.create_context()
+
+with dpg.window(tag='Goal'):
+    def addgoal(sender, app_data):
+        newgoal = dpg.get_value('New goal')
+        dpg.add_selectable(label=newgoal, parent='Goal')
+        print(dpg.get_value('New goal'))
+
+    dpg.add_text('Hi, Artem')
+    theinput = dpg.add_input_text(label='string', default_value='Enter your name...', tag='New goal')
+    dpg.add_button(label='Save', callback=addgoal)
+    dpg.add_selectable(label='Create a new app')
+
+dpg.create_viewport(title='My goals', width=600, height=600)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window('Goal', True)
+dpg.start_dearpygui()
+dpg.destroy_context()
